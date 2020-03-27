b9ebb2cef2de16071cadf02e9f3aad9338d7aff0
aakash-v-virani <aakash.v.virani@intel.com>
2020-03-27 10:34:34 +0530

    RNC-1.2 release
    Signed-off-by: aakash-v-virani <aakash.v.virani@intel.com>

---
+++ profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Copyright (C) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+# User can execute specific tasks which needs to be performed while installation using this script
